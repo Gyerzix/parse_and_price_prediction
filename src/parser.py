8a40fa7e499ba9b130ac2326c419b27f4b04f942
shakh <shahingadgiev@gmail.com>
2023-09-30 17:26:42 +0300

    changed location resource

--- src/parser.py
+++ src/parser.py
@@ -110,68 +110,67 @@ def prep_loc(locs):
 
 
 try:
-    # for name, url in all_ads_dict.items():
-    url = "https://avito.ru/moskva/kvartiry/4-k._kvartira_1065m_2323et._3071487275"
-    driver.get(url)
-    time.sleep(5)
-    driver.execute_script("window.stop();")
-    src = driver.page_source
-    soup = BeautifulSoup(src, "lxml")
+    for name, url in all_ads_dict.items():
+        driver.get(url)
+        time.sleep(5)
+        driver.execute_script("window.stop();")
+        src = driver.page_source
+        soup = BeautifulSoup(src, "lxml")
 
-    ID = soup.find('span', {'data-marker': 'item-view/item-id'}).text.split('\xa0')[1]
-    locations = soup.find_all(class_="style-item-address-georeferences-item-interval-ujKs2")  # all locations
-    location = prep_loc(locations)
-    price = int("".join(soup.find('span', {'data-marker': 'item-view/item-price'}).text.split('\xa0')))
-    list_of_price_per_m2 = soup.find(class_="style-item-price-sub-price-_5RUD").text.split('\xa0')
-    price_per_m2 = int(list_of_price_per_m2[0] + list_of_price_per_m2[1])
+        ID = soup.find('span', {'data-marker': 'item-view/item-id'}).text.split('\xa0')[1]
+        locations = soup.find_all(class_="style-item-address-georeferences-item-interval-ujKs2")  # all locations
+        location = prep_loc(locations)
+        price = int("".join(soup.find('span', {'data-marker': 'item-view/item-price'}).text.split('\xa0')))
+        list_of_price_per_m2 = soup.find(class_="style-item-price-sub-price-_5RUD").text.split('\xa0')
+        price_per_m2 = int(list_of_price_per_m2[0] + list_of_price_per_m2[1])
 
-    flat_features = soup.find_all(class_="params-paramsList__item-appQw")
-    room_count = get_item(flat_features, "Количество комнат")
-    type_of_rooms = get_item(flat_features, "Тип комнат")
-    full_area = get_item(flat_features, "Общая площадь")
-    kitchen_area = get_item(flat_features, "Площадь кухни")
-    living_area = get_item(flat_features, "Жилая площадь")
-    flat_floor = get_item(flat_features, "Этаж")[0]
-    balcony = get_item(flat_features, "Балкон или лоджия")
-    ceiling_height = get_item(flat_features, "Высота потолков")
-    bathroom = get_item(flat_features, "Санузел")
-    windows = get_item(flat_features, "Окна")
-    renovation = get_item(flat_features, "Ремонт")
-    warm_floor = get_item(flat_features, "Тёплый пол")
-    furniture = get_item(flat_features, "Мебель")
-    technic = get_item(flat_features, "Техника")
+        flat_features = soup.find_all(class_="params-paramsList__item-appQw")
+        room_count = get_item(flat_features, "Количество комнат")
+        type_of_rooms = get_item(flat_features, "Тип комнат")
+        full_area = get_item(flat_features, "Общая площадь")
+        kitchen_area = get_item(flat_features, "Площадь кухни")
+        living_area = get_item(flat_features, "Жилая площадь")
+        flat_floor = get_item(flat_features, "Этаж")[0]
+        balcony = get_item(flat_features, "Балкон или лоджия")
+        ceiling_height = get_item(flat_features, "Высота потолков")
+        bathroom = get_item(flat_features, "Санузел")
+        windows = get_item(flat_features, "Окна")
+        renovation = get_item(flat_features, "Ремонт")
+        warm_floor = get_item(flat_features, "Тёплый пол")
+        furniture = get_item(flat_features, "Мебель")
+        technic = get_item(flat_features, "Техника")
 
-    house_features = soup.find_all(class_="style-item-params-list-item-aXXql")
-    type_of_house = get_item(house_features, "Тип дома")
-    year_of_build = get_item(house_features, "Год постройки")
-    house_floors = get_item(house_features, "Этажей в доме")
-    passenger_elevator = get_item(house_features, "Пассажирский лифт")
-    freight_elevator = get_item(house_features, "Грузовой лифт")
-    passenger_elevator = int(passenger_elevator) if passenger_elevator not in ("нет", np.nan) else 0
-    freight_elevator = int(freight_elevator) if freight_elevator not in ("нет", np.nan) else 0
-    yard = get_item(house_features, "Двор")
-    parking = get_item(house_features, "Парковка")
+        house_features = soup.find_all(class_="style-item-params-list-item-aXXql")
+        type_of_house = get_item(house_features, "Тип дома")
+        year_of_build = get_item(house_features, "Год постройки")
+        house_floors = get_item(house_features, "Этажей в доме")
+        passenger_elevator = get_item(house_features, "Пассажирский лифт")
+        freight_elevator = get_item(house_features, "Грузовой лифт")
+        passenger_elevator = int(passenger_elevator) if passenger_elevator not in ("нет", np.nan) else 0
+        freight_elevator = int(freight_elevator) if freight_elevator not in ("нет", np.nan) else 0
+        yard = get_item(house_features, "Двор")
+        parking = get_item(house_features, "Парковка")
 
-    address = soup.find(class_="style-item-address__string-wt61A").text
+        address = soup.find(class_="style-item-address__string-wt61A").text
 
-    driver.get("https://yandex.ru/maps/")
-    time.sleep(5)
-    address_field = driver.find_element(By.XPATH, "//input[@dir='auto']")
-    address_field.click()
-    address_field.send_keys(address)  # для того, чтобы ввести в поле значение
-    address_field.send_keys(Keys.ENTER)
-    time.sleep(5)
-    geolocation = driver.find_elements(By.CLASS_NAME, "toponym-card-title-view__coords-badge")[0].text
-    geo_list = geolocation.split(", ")
-    geo_lat, geo_lon = geo_list[0], geo_list[1]
+        driver.get("https://dadata.ru/product/geocode/")
+        time.sleep(2)
+        address_field = driver.find_element(By.CLASS_NAME, "custom-input__input")
+        address_field.clear()
+        address_field.send_keys(address)  # для того, чтобы ввести в поле значение
+        address_field.send_keys(Keys.ENTER)
+        time.sleep(2)
+        geolocation = driver.find_element(By.CLASS_NAME, "grid-2-cols--withMobile").text.split("\n")
+        geo_lat = float(geolocation[1])
+        geo_lon = float(geolocation[3])
 
-    with open(f"../data/avito_ads.csv", "a", newline='',
-              encoding="utf-8") as file:
-        writer = csv.writer(file, delimiter=';')
-        writer.writerow((ID, room_count, type_of_rooms, full_area, living_area, flat_floor,
-                         balcony, ceiling_height, bathroom, windows, renovation, warm_floor,
-                         furniture, technic, location, geo_lat, geo_lon, year_of_build, house_floors,
-                         passenger_elevator, freight_elevator, yard, parking, price_per_m2, price))
+        with open(f"../data/avito_ads.csv", "a", newline='',
+                  encoding="utf-8") as file:
+            writer = csv.writer(file, delimiter=';')
+            writer.writerow((ID, room_count, type_of_rooms, full_area, living_area, flat_floor,
+                             balcony, ceiling_height, bathroom, windows, renovation, warm_floor,
+                             furniture, technic, location, geo_lat, geo_lon, year_of_build, house_floors,
+                             passenger_elevator, freight_elevator, yard, parking, price_per_m2, price))
 
 except Exception as ex:
     print(ex)
